e127ac18b23342c7bff19de5573735e784c5dc99
CrescentKaz <121899324+CrescentKaz@users.noreply.github.com>
2025-02-20 11:36:06 -0600

    commit

--- script.js
+++ script.js
@@ -2,17 +2,20 @@ const btn1 = document.querySelector("#btn1");
 const btn2 = document.querySelector("#btn2");
 const btn3 = document.querySelector("#btn3");
 const btn4 = document.querySelector("#btn4"); 
+const btn5 = document.querySelector("#btn5");
 
 btn1.onclick = homeFunction;
 btn2.onclick = menuFunction;
 btn3.onclick = gamesFunction;
 btn4.onclick = staffFunction;
+btn5.onclick = affiliationsFunction;
 
 function homeFunction() {
     home_section.style.display = "contents";
     menu_section.style.display = "none";
     games_section.style.display = "none";
     staff_section.style.display = "none";
+    affiliations_section.style.display = "none";
 }
 
 function menuFunction() {
@@ -20,6 +23,7 @@ function menuFunction() {
     menu_section.style.display = "contents";
     games_section.style.display = "none";
     staff_section.style.display = "none";
+    affiliations_section.style.display = "none";
 }
 
 function gamesFunction() {
@@ -27,6 +31,7 @@ function gamesFunction() {
     menu_section.style.display = "none";
     games_section.style.display = "contents";
     staff_section.style.display = "none";
+    affiliations_section.style.display = "none";
 }
 
 function staffFunction() {
@@ -34,4 +39,13 @@ function staffFunction() {
     menu_section.style.display = "none";
     games_section.style.display = "none";
     staff_section.style.display = "contents";
+    affiliations_section.style.display = "none";
+}
+
+function affiliationsFunction() {
+    home_section.style.display = "none";
+    menu_section.style.display = "none";
+    games_section.style.display = "none";
+    staff_section.style.display = "none";
+    affiliations_section.style.display = "contents";
 }
