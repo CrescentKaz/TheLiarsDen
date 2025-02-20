21ed2eb62f9cf113f760c8f8d4adb81ca5f8eec0
CrescentKaz <121899324+CrescentKaz@users.noreply.github.com>
2025-02-19 21:22:35 -0600

    commit

---
+++ script.js
@@ -0,0 +1,37 @@
+const btn1 = document.querySelector("#btn1");
+const btn2 = document.querySelector("#btn2");
+const btn3 = document.querySelector("#btn3");
+const btn4 = document.querySelector("#btn4"); 
+
+btn1.onclick = homeFunction;
+btn2.onclick = menuFunction;
+btn3.onclick = gamesFunction;
+btn4.onclick = staffFunction;
+
+function homeFunction() {
+    home_section.style.display = "contents";
+    menu_section.style.display = "none";
+    games_section.style.display = "none";
+    staff_section.style.display = "none";
+}
+
+function menuFunction() {
+    home_section.style.display = "none";
+    menu_section.style.display = "contents";
+    games_section.style.display = "none";
+    staff_section.style.display = "none";
+}
+
+function gamesFunction() {
+    home_section.style.display = "none";
+    menu_section.style.display = "none";
+    games_section.style.display = "contents";
+    staff_section.style.display = "none";
+}
+
+function staffFunction() {
+    home_section.style.display = "none";
+    menu_section.style.display = "none";
+    games_section.style.display = "none";
+    staff_section.style.display = "contents";
+}
